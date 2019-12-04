5784d20d42756d507832a1cbfa9d7c975361432e
Alexander Harms <a.g.j.harms@gmail.com>
2019-12-04 15:11:34 +0100

    Refactors the code to unify the code base between single and multi label labeling

--- cursedlabeling.py
+++ cursedlabeling.py
@@ -5,11 +5,7 @@ import argparse
 
 import pandas as pd
 
-def single_label(stdscr):
-    # Clear screen
-    stdscr.clear()
-    num_rows, num_cols = stdscr.getmaxyx()
-
+def generate_instr_window(num_rows, num_cols):
     # Create a window for instructions
     # lines, columns, start line, start column
     instr_rows = 3
@@ -19,15 +15,37 @@ def single_label(stdscr):
 
     instr_window = curses.newwin(instr_rows, instr_cols, 
                                  instr_rows_loc, instr_cols_loc)
+    instr_win_prop = [instr_rows, instr_cols, 
+                      instr_rows_loc, instr_cols_loc]
+    return instr_window, instr_win_prop
 
+def generate_text_window(num_rows, num_cols,
+                         instr_rows = 3):
     # Create a window for text
     # lines, columns, start line, start column
     text_rows = num_rows - instr_rows - 1
     text_cols = num_cols - 1
     text_rows_loc = 0
     text_cols_loc = 0
+
     text_window = curses.newwin(text_rows, text_cols, 
                                 text_rows_loc, text_cols_loc)
+    
+    text_win_prop = [text_rows, text_cols,
+                     text_rows_loc, text_cols_loc]
+    return text_window, text_win_prop
+
+def single_label(stdscr):
+    # Clear screen
+    stdscr.clear()
+    num_rows, num_cols = stdscr.getmaxyx()
+
+    # Create a window for instructions and for the text
+    instr_window, instr_win_prop = generate_instr_window(num_rows, num_cols)
+    text_window, text_win_prop = generate_text_window(num_rows, num_cols,
+                                                      instr_win_prop[0])
+    text_rows = text_win_prop[0]
+    text_cols = text_win_prop[1]
 
     press = 1
     text_index = 0
@@ -35,6 +53,12 @@ def single_label(stdscr):
     response_list = []
 
     stdscr.refresh()
+
+    # Create a window for instructions
+    instr_window.addstr(0, 0, "Single label. Press 'q' to close")
+    instr_window.refresh()
+
+    # Start the labeling procedure
     while True:
         text_window.clear()
         # Fill window for text
@@ -44,10 +68,6 @@ def single_label(stdscr):
 
         text_window.addstr(0, 0, text)
         text_window.refresh()
-
-        # Create a window for instructions
-        instr_window.addstr(0, 0, "Single label. Press 'q' to close")
-        instr_window.refresh()
         
         # Get input
         press = stdscr.getch()
@@ -71,31 +91,25 @@ def multi_label(stdscr):
     stdscr.clear()
     num_rows, num_cols = stdscr.getmaxyx()
 
-    # Create a window for instructions
-    # lines, columns, start line, start column
-    instr_rows = 3
-    instr_cols = num_cols - 1
-    instr_rows_loc = num_rows - 1 - instr_rows
-    instr_cols_loc = 0
-
-    instr_window = curses.newwin(instr_rows, instr_cols, 
-                                 instr_rows_loc, instr_cols_loc)
-
-    # Create a window for text
-    # lines, columns, start line, start column
-    text_rows = num_rows - instr_rows - 1
-    text_cols = num_cols - 1
-    text_rows_loc = 0
-    text_cols_loc = 0
-    text_window = curses.newwin(text_rows, text_cols, 
-                                text_rows_loc, text_cols_loc)
+    # Create a window for instructions and for the text
+    instr_window, instr_win_prop = generate_instr_window(num_rows, num_cols)
+    text_window, text_win_prop = generate_text_window(num_rows, num_cols,
+                                                      instr_win_prop[0])
+    text_rows = text_win_prop[0]
+    text_cols = text_win_prop[1]
 
     press = 1
     text_index = 0
     text_array = pd.read_csv(data_filename)
     response_list = []
+    response_press = []
 
     stdscr.refresh()
+
+    # Create a window for instructions
+    instr_window.addstr(0, 0, "Multi-label. Press 'n' for next text. " 
+            + "Press 'q' to close")
+    instr_window.refresh()
     while True:
         text_window.clear()
         # Fill window for text
@@ -106,19 +120,20 @@ def multi_label(stdscr):
         text_window.addstr(0, 0, text)
         text_window.refresh()
 
-        # Create a window for instructions
-        instr_window.addstr(0, 0, "Multi-label. Press 'q' to close")
-        instr_window.refresh()
         
         # Get input
         press = stdscr.getch()
         
         if press is ord("q"):
             break
+        elif press is ord("n"):
+            response_list.append(response_press)
+            response_press = []
+            text_index += 1
+        else:
+            # Add response to the list
+            response_press.append(chr(press))
 
-        # Add response to the list
-        response_list.append(chr(press))
-        text_index += 1
         # If all the texts are labeled, break
         if text_index >= text_array.shape[0]:
             break
